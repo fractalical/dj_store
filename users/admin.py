e49c0591aaf9a5d6279b533757b208ac04c93fdd
Artur <ArthurZ43@yandex.ru>
2023-01-17 22:11:41 +0300

    edit admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
+from products.admin import BasketAdmin
 from users.models import User
 
+
 # Register your models here.
-admin.site.register(User)
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmon(admin.ModelAdmin):
+    list_display = ('username',)
+    inlines = (BasketAdmin,)
