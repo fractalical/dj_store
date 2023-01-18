f505f27d8aae5f0f5656d4d5ef7163e175c25c42
Artur <ArthurZ43@yandex.ru>
2023-01-18 21:31:36 +0300

    edited views from func to class

--- products/views.py
+++ products/views.py
@@ -1,31 +1,28 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from products.models import Product, ProductCategory, Basket
-from django.core.paginator import Paginator
+from common.views import TitleMixin
 
 
-# Create your views here.
-def index(request):
-    context = {
-        'title': 'Store',
-        'username': 'artur',
-        'is_promotion': True,
-    }
-    return render(request, 'products/index.html', context)
+class IndexView(TitleMixin, TemplateView):
+    template_name = 'products/index.html'
+    title = 'Store'
 
 
-def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    per_page = 3
-    paginator = Paginator(products, per_page=per_page)
-    products_paginator = paginator.page(page_number)
-    context = {
-        'title': 'Store - Каталог',
-        'products': products_paginator,
-        'categories': ProductCategory.objects.all(),
-    }
-    return render(request, 'products/products.html', context)
+class ProductsListView(TitleMixin, ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+    title = 'Store - Каталог'
+
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
 
 @login_required
 def basket_add(request, product_id):
